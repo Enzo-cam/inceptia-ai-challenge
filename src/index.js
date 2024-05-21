d64d1ee0cb6614031a08e60e74beaf320c33f4b0
Enzo-cam <camerac09@gmail.com>
2024-05-21 19:16:08 -0300

    Main

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-import "core-js/stable";
-import "regenerator-runtime/runtime";
-import "react-app-polyfill/ie11";
-import ReactDOM from "react-dom";
+import './index.css';
+import { StrictMode } from "react";
+import reactDom from "react-dom";
 import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from './redux/store';
+import App from './App';
 
-ReactDOM.render(
-  "Welcome to Inceptia React Challenge",
-  document.getElementById("root")
+
+const rootElement = document.getElementById("root");
+  
+reactDom.render(
+    <StrictMode>
+      <BrowserRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
+    </StrictMode>,
+    rootElement
 );
 
 serviceWorker.unregister();
