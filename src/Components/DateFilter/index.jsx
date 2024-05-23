c48a9810fa4b84b19082a7ed4ebb6a8d1d2a3c01
Enzo-cam <camerac09@gmail.com>
2024-05-23 16:46:05 -0300

    Feat: changes at UI

--- src/Components/DateFilter/index.jsx
+++ src/Components/DateFilter/index.jsx
@@ -8,10 +8,10 @@ const Filters = () => {
     const { from, until } = useSelector((state) => state.clients.dateFilter);
 
     return (
-        <div className="flex space-x-4 p-2 bg-white shadow rounded-lg justify-center items-center">
+        <div className="flex space-x-4 p-2 bg-white shadow rounded-lg justify-center border items-center">
             <p>Desde </p>
             <input
-                className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                className="block w-full px-3 py-2 border border-gray-500 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 type="date"
                 value={from}
                 onChange={(e) => dispatch(setFromDate(e.target.value))}
@@ -19,7 +19,7 @@ const Filters = () => {
 
             <p>Hasta</p>
             <input
-                className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                className="block w-full px-3 py-2 border border-gray-500 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 type="date"
                 value={until}
                 onChange={(e) => dispatch(setUntilDate(e.target.value))}
